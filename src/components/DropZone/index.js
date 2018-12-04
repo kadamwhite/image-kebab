40d4783f7cb4e88060a08e31f8886c68c6328cad
K. Adam White <kadamwhite@users.noreply.github.com>
2018-12-04 01:01:09 -0500

    Swap out Dropzone for DropToUpload

--- src/components/DropZone/index.js
+++ src/components/DropZone/index.js
@@ -1,43 +1,77 @@
-import React, { PureComponent } from 'react'; 
-import Dropzone from 'react-dropzone';
+import DropToUpload from 'react-drop-to-upload';
+import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react';
 
 import './style.css';
 
+const loadImage = src => new Promise( ( resolve, reject ) => {
+	const img = new Image();
+	img.onload = function() {
+		resolve( {
+			width: this.width,
+			height: this.height,
+			src,
+		} );
+	};
+	img.onerror = ( err ) => reject( err );
+
+	// Start loading the image
+	img.src = src;
+} );
+
+const readAsImage = file => new Promise( ( resolve, reject ) => {
+	const reader = new FileReader();
+	reader.onload = evt => {
+		loadImage( evt.target.result )
+			.then( resolve )
+			.catch( reject );
+	};
+	reader.onabort = () => reject( 'File read aborted' );
+	reader.onerror = () => reject( 'File read failed' );
+
+	// Start reading the file
+	reader.readAsDataURL( file );
+} );
+
 class DropZone extends PureComponent {
-  constructor( props ) {
-    super( props );
-    this.onDrop = this.onDrop.bind( this );
-  }
+	constructor( props ) {
+		super( props );
+		this.onDrop = this.onDrop.bind( this );
+		this.onFileChange = this.onFileChange.bind( this );
+	}
 
-  onDrop( acceptedFiles ) {
-    const parentOnDrop = this.props.onDrop;
-    acceptedFiles.forEach( file => {
-      const reader = new FileReader();
-      reader.onload = e => {
-        const img = new Image();
-        img.onload = function() {
-          parentOnDrop( {
-            src: img.src,
-            width: this.width,
-            height: this.height,
-          } );
-        };
-        img.src = e.target.result;
-      };
-      reader.onabort = () => console.log( 'file reading was aborted' );
-      reader.onerror = () => console.log( 'file reading has failed' );
+	onDrop( acceptedFiles ) {
+		readAsImage( acceptedFiles[ 0 ] ).then( image => {
+			console.log( 'Loaded!' );
+			this.props.onDrop( image );
+		} );
+	}
 
-      reader.readAsDataURL( file );
-    } );
-  }
+	onFileChange( evt ) {
+		readAsImage( evt.target.files[0] ).then( image => {
+			console.log( 'Loaded!' );
+			this.props.onDrop( image );
+		} );
+	}
 
-  render() {
-    return (
-      <Dropzone onDrop={ this.onDrop }>
-        <div className="dropzone-placeholder" />
-      </Dropzone>
-    );
-  }
+	render() {
+		return (
+			<DropToUpload
+				dropEffect="copy"
+				onDrop={ this.onDrop }
+			>
+				<div className="dropzone-placeholder" />
+				<input
+					type="file"
+					onChange={ this.onFileChange }
+				/>
+			</DropToUpload>
+		);
+	}
 }
+
+DropZone.propTypes = {
+	onDrop: PropTypes.func.isRequired,
+};
 
 export default DropZone;
